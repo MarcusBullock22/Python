1dab2e19c81753bfb80373dbf3b615c6f1b0175c
ThexTallxDude <124476536+ThexTallxDude@users.noreply.github.com>
2025-01-29 20:54:51 -0500

    Update MeowBot.py
    Files now come from Backblaze bucket rather than local files

--- CatGirlBot/MeowBot.py
+++ CatGirlBot/MeowBot.py
@@ -1,3 +1,4 @@
+from b2sdk.v1 import B2Api, InMemoryAccountInfo
 import random
 import discord
 from discord.ext import commands
@@ -10,6 +11,15 @@ bot.remove_command('help')
 
 last_image_path = None  # Variable to store the path of the last image sent
 
+#Backblaze stuff
+account_id = ""
+application_key = ""
+info = InMemoryAccountInfo()
+b2_api = B2Api(info)
+b2_api.authorize_account("production", account_id, application_key)
+bucket = b2_api.get_bucket_by_name("")
+
+
 @bot.event
 async def on_message(message):
     global last_image_path  # Access the last_image_path variable
@@ -17,83 +27,50 @@ async def on_message(message):
     if not message.author.bot:
         pattern1 = r"(?i)\bMeow\b"
         pattern2 = r"(?i)\bFemboy\b"
-        pattern3= r"(?i)\bUwu\b"
+        pattern3 = r"(?i)\bUwu\b"
+        pattern4 = r"(?i)\bdipshitdev\b"
 
     #Standard
         if re.search(pattern1, message.content):
             if "bot" in message.channel.name.lower():
-                folder_path = "/DiscordBots/Images/CatGirls"
-                file_names = os.listdir(folder_path)
-                image_paths = [
-                    os.path.join(folder_path, file_name) for file_name in file_names
-                ]
+                image_paths = []
+                for file_version_info, folder_name in bucket.ls(folder_to_list='Meow'):
+                    image_paths.append(file_version_info.file_name)
                 if image_paths:
                     random_image_path = random.choice(image_paths)
-                    with open(random_image_path, "rb") as image_file:
-                        image = discord.File(image_file)
-                        await message.channel.send(file=image)
-                        last_image_path = random_image_path  # Store the path of the last image sent
-                        print("Last image path:", last_image_path)
+                    await message.channel.send(b2_api.get_download_url_for_file_name(bucket_name="",file_name=random_image_path))
+                    last_image_path = random_image_path  # Store the path of the last image sent
+                    print("Last image path:", last_image_path)
                 else:
                     await message.channel.send("No images found.")
   
     #With Dicks
         if re.search(pattern2, message.content):
             if "bot" in message.channel.name.lower():
-                folder_path = "/DiscordBots/Images/FemBoy"
-                file_names = os.listdir(folder_path)
-                image_paths = [
-                    os.path.join(folder_path, file_name) for file_name in file_names
-                ]
+                image_paths = []
+                for file_version_info, folder_name in bucket.ls(folder_to_list='FemBoy'):
+                    image_paths.append(file_version_info.file_name)
                 if image_paths:
                     random_image_path = random.choice(image_paths)
-                    with open(random_image_path, "rb") as image_file:
-                        image = discord.File(image_file)
-                        await message.channel.send(file=image)
-                        last_image_path = random_image_path  # Store the path of the last image sent
-                        print("Last image path:", last_image_path)
+                    await message.channel.send(b2_api.get_download_url_for_file_name(bucket_name="",file_name=random_image_path))
+                    last_image_path = random_image_path  # Store the path of the last image sent
+                    print("Last image path:", last_image_path)
                 else:
-                    await message.channel.send("No images found.")     
-
-    #Ff Degens
+                    await message.channel.send("No images found.")
+    
+    #Ff Degens   
         if re.search(pattern3, message.content):
             if "bot" in message.channel.name.lower():
-                folder_path = "/DiscordBots/Images/Uwu"
-                file_names = os.listdir(folder_path)
-                image_paths = [
-                    os.path.join(folder_path, file_name) for file_name in file_names
-                ]
+                image_paths = []
+                for file_version_info, folder_name in bucket.ls(folder_to_list='uwu'):
+                    image_paths.append(file_version_info.file_name)
                 if image_paths:
                     random_image_path = random.choice(image_paths)
-                    with open(random_image_path, "rb") as image_file:
-                        image = discord.File(image_file)
-                        await message.channel.send(file=image)
-                        last_image_path = random_image_path  # Store the path of the last image sent
-                        print("Last image path:", last_image_path)
+                    await message.channel.send(b2_api.get_download_url_for_file_name(bucket_name="",file_name=random_image_path))
+                    last_image_path = random_image_path  # Store the path of the last image sent
+                    print("Last image path:", last_image_path)
                 else:
-                    await message.channel.send("No images found.")  
+                    await message.channel.send("No images found.")
     await bot.process_commands(message)
-
-@bot.command(name='killkitty')
-async def killkitty(ctx):
-    global last_image_path  # Access the last_image_path variable
-    print("Got to Kill Command")
-    if "bot" in ctx.channel.name.lower():
-            print("Made it into first if")
-            # Extract the filename from the path
-            image_filename = os.path.basename(last_image_path)
-            # Construct the full file path in the CatGirls folder
-            catgirls_folder_path = "/DiscordBots/Images/CatGirls"
-            image_file_path = os.path.join(catgirls_folder_path, image_filename)
-
-            if os.path.exists(image_file_path):
-                os.remove(image_file_path)  # Delete the image file from the CatGirls folder
-                await ctx.send("Last image removed.")
-                print("Attempted to delete:", last_image_path)
-            else:
-                await ctx.send("Last image file not found.")
-    else:
-        await ctx.send("No image to remove.")
-
 bot.run(
   "")
